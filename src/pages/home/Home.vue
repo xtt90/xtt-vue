cfdaa583d6bf3783b40403da282cf8355fb1bd11
Your Namextt90 <420481754@qq.com>
2019-04-29 14:05:03 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='home'>
+        <div >hello world</div>
+        <router-Link to="/list">列表</router-Link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+    .home{
+        font-size: 50px;
+    }
+</style>
